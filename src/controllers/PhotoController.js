607684760d81e29e7ff82b62f2d010ed0545f822
Vinicius Tanaka <vi_massaru.tanaka@hotmail.com>
2021-10-23 02:08:35 -0300

    setting-controllers-and-models-to-save-in-DB

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,6 +1,24 @@
+import multer from 'multer';
+
+import multerConfig from '../config/multerConfig';
+import Photo from '../models/Photo';
+
+const upload = multer(multerConfig).single('archive');
+
 class PhotoController {
-  async store(req, answr) {
-    answr.json(req.file);
+  store(req, answr) {
+    return upload(req, answr, async (error) => {
+      if (error) {
+        return answr.status(400).json({
+          error: [error.code],
+        });
+      }
+
+      const { originalname, filename } = req.file;
+      const { student_id } = req.body;
+      const photo = await Photo.create({ originalname, filename, student_id });
+      return answr.json(photo);
+    });
   }
 }
 
